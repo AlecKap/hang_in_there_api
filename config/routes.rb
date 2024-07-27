52ca215e583c37ac829015042f29901a981dd3ac
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-07-27 13:30:56 -0600

    feat: Add API endpoint for creating a new poster

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/posters', to: 'posters#index'
       get '/poster/:id', to: 'posters#show'
+      post '/posters', to: 'posters#create'
     end
   end
 end
