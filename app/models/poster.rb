ceb9de055da2228d67c1749cd0e3c82168420896
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-07-27 14:44:45 -0600

    feat: Add methods for ordering posters by created date

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -1,2 +1,10 @@
 class Poster < ApplicationRecord
+
+  def self.order_by_created_at_asc
+    order(created_at: :asc)
+  end
+
+  def self.order_by_created_at_desc
+    order(created_at: :desc)
+  end
 end
