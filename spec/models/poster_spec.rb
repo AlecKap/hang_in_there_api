a7fb60e023ad4b41b4fc212be932d1f346fff099
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-07-27 15:20:40 -0600

    test: Write test for #search_by_name

--- spec/models/poster_spec.rb
+++ spec/models/poster_spec.rb
@@ -26,17 +26,31 @@ RSpec.describe Poster, type: :model do
                                 vintage: true,
                                 img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d",
                                 created_at: 3.days.ago)
-    end
-    
+        
+        @poster4 = Poster.create(name: "SUCCESS",
+                                 description: "You did it!",
+                                 price: 98.00,
+                                 year: 2010,
+                                 vintage: true,
+                                 img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d",
+                                 created_at: 4.days.ago)                        
+      end
+
     describe '.order_by_created_at_asc' do
       it 'orders posters by created_at in ascending order' do
-        expect(Poster.order_by_created_at_asc).to eq([@poster3, @poster2, @poster1])
+        expect(Poster.order_by_created_at_asc).to eq([@poster4, @poster3, @poster2, @poster1])
       end
     end
 
     describe '.order_by_created_at_desc' do
       it 'orders posters by created_at in descending order' do
-        expect(Poster.order_by_created_at_desc).to eq([@poster1, @poster2, @poster3])
+        expect(Poster.order_by_created_at_desc).to eq([@poster1, @poster2, @poster3, @poster4])
+      end
+    end
+
+    describe '.search_by_name' do
+      it 'returns posters that match the search term' do
+        expect(Poster.search_by_name("ce")).to eq([@poster3, @poster4])
       end
     end
   end
