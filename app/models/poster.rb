48f5c3f5e94896c0249e05efbfc32deb560cd92f
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-07-27 15:34:30 -0600

    feat: Add filter methods for min and max price to Poster model

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -11,4 +11,12 @@ class Poster < ApplicationRecord
   def self.search_by_name(search)
     where('name ILIKE ?', "%#{search}%").order(:name)
   end
+
+  def self.filter_by_max_price(max_price)
+    where('price <= ?', max_price).order(:price)
+  end
+  
+  def self.filter_by_min_price(min_price)
+    where('price >= ?', min_price).order(:price)
+  end
 end
