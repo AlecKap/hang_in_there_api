3524a339277efcfa67e9e02aecb75879ea26ad3a
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-07-27 14:34:42 -0600

    feat: Add meta method to PostersSerializer for counting posters

--- app/serializers/posters_serializer.rb
+++ app/serializers/posters_serializer.rb
@@ -1,4 +1,8 @@
 class PostersSerializer
   include JSONAPI::Serializer
   attributes :name, :description, :price, :year, :vintage, :img_url
+
+  def self.meta(posters)
+    { count: posters.count }
+  end
 end
