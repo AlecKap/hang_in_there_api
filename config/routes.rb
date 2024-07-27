596745bd12371dfe5c6f1a1955ba146c0988427e
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-07-27 13:35:46 -0600

    feat: Add patch action to posters controller for updating existing posters

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       get '/posters', to: 'posters#index'
       get '/poster/:id', to: 'posters#show'
       post '/posters', to: 'posters#create'
+      patch '/posters/:id', to: 'posters#update'
     end
   end
 end
