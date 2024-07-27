798b80fa5d9db7c3cc4f23f5e7fdc3f61d8e8c5d
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-07-27 15:19:46 -0600

    Refactor Poster model to add search functionality by name

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -7,4 +7,8 @@ class Poster < ApplicationRecord
   def self.order_by_created_at_desc
     order(created_at: :desc)
   end
+
+  def self.search_by_name(search)
+    where('name ILIKE ?', "%#{search}%").order(:name)
+  end
 end
