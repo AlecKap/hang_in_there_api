57aa93ec62509eedfccb68254a96b2bae8763f89
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-08-13 17:19:28 -0600

    test: Add test data for posters in spec file

---
+++ spec/test_data.rb
@@ -0,0 +1,29 @@
+def posters_test_data
+  Poster.create(name: "REGRET",
+              description: "Hard work rarely pays off.",
+              price: 89.00,
+              year: 2018,
+              vintage: true,
+              img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+
+  Poster.create(name: "MOTIVATION",
+                description: "You can do it!",
+                price: 77.00,
+                year: 2017,
+                vintage: true,
+                img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+
+  Poster.create(name: "RESILIENCE",
+                description: "Stick with it!",
+                price: 87.00,
+                year: 2015,
+                vintage: true,
+                img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+
+  Poster.create(name: "SUCCESS",
+                description: "You did it!",
+                price: 98.00,
+                year: 2010,
+                vintage: true,
+                img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+end
