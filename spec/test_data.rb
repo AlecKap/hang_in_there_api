655bde273d3c537ef75455de0256d5c8c3e20c0f
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-08-16 13:19:36 -0600

    refactor: Update test data creation in spec file

--- spec/test_data.rb
+++ spec/test_data.rb
@@ -1,29 +1,33 @@
 def posters_test_data
-  Poster.create(name: "REGRET",
-              description: "Hard work rarely pays off.",
-              price: 89.00,
-              year: 2018,
-              vintage: true,
-              img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+  @poster1 = Poster.create(name: "REGRET",
+                          description: "Hard work rarely pays off.",
+                          price: 89.00,
+                          year: 2018,
+                          vintage: true,
+                          img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d",
+                          created_at: 1.days.ago)
 
-  Poster.create(name: "MOTIVATION",
-                description: "You can do it!",
-                price: 77.00,
-                year: 2017,
-                vintage: true,
-                img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+  @poster2 = Poster.create(name: "MOTIVATION",
+                          description: "You can do it!",
+                          price: 77.00,
+                          year: 2017,
+                          vintage: true,
+                          img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d",
+                          created_at: 2.days.ago)
 
-  Poster.create(name: "RESILIENCE",
-                description: "Stick with it!",
-                price: 87.00,
-                year: 2015,
-                vintage: true,
-                img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+  @poster3 = Poster.create(name: "RESILIENCE",
+                          description: "Stick with it!",
+                          price: 87.00,
+                          year: 2015,
+                          vintage: true,
+                          img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d",
+                          created_at: 3.days.ago)
 
-  Poster.create(name: "SUCCESS",
-                description: "You did it!",
-                price: 98.00,
-                year: 2010,
-                vintage: true,
-                img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+  @poster4 = Poster.create(name: "SUCCESS",
+                          description: "You did it!",
+                          price: 98.00,
+                          year: 2010,
+                          vintage: true,
+                          img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d",
+                          created_at: 4.days.ago)
 end
