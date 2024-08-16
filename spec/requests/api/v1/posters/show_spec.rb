98cfe491917d3594792952da2ae2912ef34e9a1d
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-08-16 14:24:56 -0600

    test: Write test for posters show request

---
+++ spec/requests/api/v1/posters/show_spec.rb
@@ -0,0 +1,47 @@
+require "rails_helper"
+require "test_data"
+
+RSpec.describe "Posters show request" do
+  describe "GET /api/v1/posters/:id" do
+    before :each do
+      posters_test_data
+    end
+    it "returns a single poster by id" do
+      id = @poster1.id
+
+      get "/api/v1/posters/#{id}"
+
+      expect(response).to be_successful
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data.count).to eq(1)
+      expect(data).to have_key(:data)
+      expect(data[:data]).to be_an(Hash)
+
+      poster = data[:data]
+      expect(poster).to have_key(:id)
+      expect(poster).to have_key(:type)
+      expect(poster).to have_key(:attributes)
+
+      expect(poster[:type]).to eq('posters')
+      expect(poster[:id]).to be_a(String)
+      expect(poster[:attributes]).to be_a(Hash)
+
+      poster_attributes = poster[:attributes]
+      expect(poster_attributes).to have_key(:name)
+      expect(poster_attributes).to have_key(:description)
+      expect(poster_attributes).to have_key(:price)
+      expect(poster_attributes).to have_key(:year)
+      expect(poster_attributes).to have_key(:vintage)
+      expect(poster_attributes).to have_key(:img_url)
+
+      expect(poster_attributes[:name]).to be_a(String)
+      expect(poster_attributes[:description]).to be_a(String)
+      expect(poster_attributes[:price]).to be_a(Float)
+      expect(poster_attributes[:year]).to be_an(Integer)
+      expect(poster_attributes[:vintage]).to be(true).or be(false)
+      expect(poster_attributes[:img_url]).to be_a(String)
+    end
+  end
+end
